ecdc998fa8c73b0d09d015fa5a5ab8a11c6785ae
Dumindu Buddhika <dumindukarunathilaka@gmail.com>
2013-10-10 15:31:54 +0530

    extra modifications

--- test/projectatlas/FactoryLogsTableProxyTest.java
+++ test/projectatlas/FactoryLogsTableProxyTest.java
@@ -70,26 +70,4 @@ public class FactoryLogsTableProxyTest {
         assertEquals(expResult, result);
     }
 
-    /**
-     * Test of getTableContents method, of class FactoryLogsTableProxy.
-     */
-    @Test
-    public void testGetTableContents() throws SQLException {
-        System.out.println("getTableContents");
-        FactoryLogsTableProxy instance = FactoryLogsTableProxy.getFactoryLogsTableProxy();
-        ResultSet expResult = null;
-        try {
-            
-            stmt=connection.createStatement();
-            ResultSet results=stmt.executeQuery("select * from factory_logs where level='INFO'");
-            expResult= results;
-        } catch (SQLException ex) {
-           expResult= null;
-        }
-       
-        ResultSet result = instance.getTableContents();
-        expResult.next();
-        result.next();
-        assertEquals(expResult.getString(4), result.getString(4));
-    }
 }
